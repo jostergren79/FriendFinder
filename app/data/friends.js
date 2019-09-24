4f24566f346754fdeb521a27905909e5c8fdc989
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-09-24 16:23:28 -0500

    initial work

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+const howMuchArray = [1,2,3,4,5]
+
+"How much you like coffee?"
+
+"How much do you like movies?"
+
+"How much do you enjoy the outdoors?"
+
+"How fun are boardgames for you?"
+
+"How much do you enjoy crossword puzzles?"
+
+"How much do you like to ride bycicles?"
+
+"How much do you enjoy reading?"
+
+"How important is music to you?"
+
+"How regularly do you shower?"
