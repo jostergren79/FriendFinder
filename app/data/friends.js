87023d50912b92bd0e3323d7e41ab44e5496a486
Jason Ostergren <jason.ostergren79@gmail.com>
2019-09-24 23:19:21 -0500

    updated readme file, modified friends.js to include additioal entries, updated npm module package imports

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,51 @@
 // example user data
 const friendArray = [
 {
-    name:"Ahmed",
+    name:"Jason Ostergren",
     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores:[
         5,
         1,
+        1,
+        2,
+        2,
+        1,
+        2,
+        2,
         4,
-        4,
+        1
+      ]
+  },
+  {
+    name:"Super Man",
+    photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjRpPL-jevkAhUMO60KHaaBBO8QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.gettyimages.com%2Fphotos%2Fsuperman&psig=AOvVaw1a1U-K9tIPXWofIRFof96B&ust=1569471205088997",
+    scores:[
+        5,
+        1,
+        5,
+        3,
         5,
         1,
         2,
+        2,
+        3,
+        3
+      ]
+  },
+  {
+    name:"Rubber Duck",
+    photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwik84CQjuvkAhURGKwKHeUOAjwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FMunchkin-White-Safety-Bath-Ducky%2Fdp%2FB000GUZC2A&psig=AOvVaw0prO-6oDuPXnWdQyglaPgC&ust=1569471240336523",
+    scores:[
+        1,
+        2,
+        4,
+        3,
         5,
         4,
-        1
+        1,
+        3,
+        2,
+        5
       ]
   }
 ];
